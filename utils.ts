d213db776a549a9ffca630293db03a135fbeea1a
Rahul_Singh <koderrahul@gmail.com>
2024-08-02 19:48:33 +0530

    fetcing api

---
+++ utils.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const fetchPosts = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+};
